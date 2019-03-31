97bbc2819fc7385af9311b09879c53b95a316b3c
Marsh <mmvettorel@gmail.com>
2019-03-31 16:37:11 -0300

    utilizacion de factory

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+				
 		Scanner entrada = new Scanner(System.in);
 		int numero;
 	
 		System.out.println("Ingrese un numero entero: ");
 		numero = entrada.nextInt();
 		
-		IFactorial calculador = new FactorialImpl();
-				
+		IFactorialFactory fabFactorial = new FactorialFactoryImpl();
+		
+		IFactorial calculador = fabFactorial.create();
+		
 		System.out.println(calculador.calcular(numero));
 				
 		entrada.close();
